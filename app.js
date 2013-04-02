d8e836fcf37dc2ad132c2fcbbc20d0c515e60611
Kevin DeRonne <deronne@samas.nxn.com>
2013-04-02 15:11:56 -0500

    all but envelopes working

--- app.js
+++ app.js
@@ -1,32 +1,25 @@
 // Bad deronne, bad.  global variables?
 var filedata;
 var svg;
-
+var imgpixel;
 var  dots_height = 700; // yextent in coleman's code
 var  dots_width  = 700; // xextent in coleman's code
+var counter      = 0;
+
+var PI = 3.1415927;
+function toradians(x) 
+{
+  return x*((2*PI)/360);
+}
+
 Ext.application({
     name: 'Dot',
     launch: function() {
-    Ext.create('Ext.form.Panel', {
+    var containerPanel = Ext.create('Ext.form.Panel', {
       width: 330,
       bodyPadding: 10,
-      //renderTo: Ext.getBody(),
       renderTo: 'dotctrl',
       items: [
-/*
-        { 
-          xtype: 'filefield',
-          name: 'image',
-          fieldLabel: 'Just jpg pretties',
-          labelWidth: 50,
-          msgTarget: 'side',
-          allowBlank: false,
-          anchor: '100%',
-          buttonText: 'Analyze Color Distribution',
-          //validateOnChange: true,
-          //validator: function() {console.log(this.up('buttons'))}
-        },
-*/
         {
           xtype: 'numberfield',
           //anchor: '100%',
@@ -105,20 +98,6 @@ Ext.application({
           increment: 1,
           maxValue: 100
         },
-/*
-        {
-          xtype: 'checkbox',
-          boxLabel: 'Massive spew?',
-          name: 'spew',
-          id: 'spewcheckbox'
-        },
-        {
-          xtype: 'checkbox',
-          boxLabel: 'Outlines?',
-          name: 'outline',
-          id: 'outlinecheckbox'
-        },
-*/
         {
           xtype: 'checkboxgroup',
           //fieldLabel: 'Two Columns',
@@ -137,7 +116,9 @@ Ext.application({
       buttons: [
         {
           text:         'Go!',
-          //disabled:     true,
+          //formBind:     true,
+          disabled:     true,
+          id: 'gobutton',
           handler:      dot_analyze
         },
         {
@@ -154,14 +135,13 @@ Ext.application({
         },
       ]
   });
-  /*
   svg  = d3.select("#dotspan").insert("svg:svg", "h2")
            .attr("height", dots_height)
            .attr("width", dots_width)
            .style("background-color","black");
-           */
  
-    }
+  //console.log(Math.toRadians());
+  }
 });
 
 function dot_clearcanvas() {
@@ -190,9 +170,6 @@ function dot_getvalues(form)
         dotspace:         Number(formvars.dotspace),
         dotregularity:    Number(formvars.dotregularity),
   }
-  // this gets you the jpg pretties box
-  ret.file = form.down().lastvalue; 
-
   // this gets the checkbox array
   boxchecked = form.down('checkboxgroup').getChecked();
 
@@ -201,51 +178,143 @@ function dot_getvalues(form)
   ret.envelope1 = 0;
   ret.envelope2 = 0;
 
-  for(i=0; i<boxchecked; i++) {
-    ret[i].name = 1;
+  for(i=0; i<boxchecked.length; i++) {
+    if(boxchecked[i].name === "random") {
+      ret.random = 1;
+    }
+    if(boxchecked[i].name === "outlines") {
+      ret.outlines = 1;
+    }
+    if(boxchecked[i].name === "envelope1") {
+      ret.envelope1 = 1;
+    }
+    if(boxchecked[i].name === "envelope2") {
+      ret.envelope2 = 1;
+    }
   }
+  //console.log(ret);
 /* }}} */
   return ret;
 }
 
-function drawDot(cx, cy, rx, ry, rgb, asoutline) 
+function drawDot(cx, cy, rx, ry, rgb, params) 
 {
   fill = rgb;
-  if(asoutline === 1) {
-    fill = "black";
+  if(params.outlines === 1) {
+    fill = "none";
   }
   svg.append("svg:ellipse").attr("cx", cx).attr("cy",cy).attr("rx",rx).attr("ry",ry).style("stroke",rgb).style("fill",fill);
+}
+
+function drawBooble(x, y, rx, ry, rgb, params ) 
+{
+  halfdotsize = params.dotsize/2.0;
+  cx = x - halfdotsize;
+  cy = y - halfdotsize;
+  rotater = 360.0/params.multibooble;
+
+  fill = rgb;
+  xranda = new Array;
+  yranda = new Array;
+  bra    = new Array;
+  for(booblecount=0; booblecount < params.multibooble; booblecount++) {
+    boobleradius = (Math.random() - 0.5) * (params.flermox * 0.2) * halfdotsize + 6.0 / params.trumble * halfdotsize;
+    xmod = halfdotsize * Math.cos(toradians(rotater * booblecount)) + x;
+    ymod = halfdotsize * Math.sin(toradians(rotater * booblecount)) + y;
+
+    xrand = Math.random();
+    yrand = Math.random();
+    
+    xranda.push(xrand);
+    yranda.push(yrand);
+    bra.push(boobleradius);
+
+    svg.append("svg:ellipse")
+    .attr("cx", xmod + (xrand - 0.5) * (params.wonk * 0.2) * halfdotsize + (halfdotsize - boobleradius))
+    .attr("cy", ymod + (yrand - 0.5) * (params.wonk * 0.2) * halfdotsize + (halfdotsize - boobleradius))
+    .attr("rx", 2.0 * boobleradius)
+    .attr("ry", 2.0 * boobleradius)
+    .style("stroke",rgb)
+    .style("stroke-width",4)
+    .style("fill",fill);
+  }
+
+  if(params.outlines === 1) {
+    for(booblecount=0; booblecount < params.multibooble; booblecount++) {
+
+      xmod = halfdotsize * Math.cos(toradians(rotater * booblecount)) + x;
+      ymod = halfdotsize * Math.sin(toradians(rotater * booblecount)) + y;
+
+      xrand        = xranda[booblecount];
+      yrand        = yranda[booblecount];
+      boobleradius = bra[booblecount];
+
+      svg.append("svg:ellipse")
+      .attr("cx", xmod + (xrand - 0.5) * (params.wonk * 0.2) * halfdotsize + (halfdotsize - boobleradius))
+      .attr("cy", ymod + (yrand - 0.5) * (params.wonk * 0.2) * halfdotsize + (halfdotsize - boobleradius))
+      .attr("rx", 2.0 * boobleradius)
+      .attr("ry", 2.0 * boobleradius)
+      .style("fill","black");
+    }
+  }
 }
 
 function drawDots(params) 
 {
 //test = dots_height+100;
 //console.log(test);
-  for(iy=params.dotsize+params.dY; iy<dots_height; iy += params.dotspace) {
-    for(ix=params.dotsize+params.dX; ix<dots_width; ix += params.dotspace) {
-      //counter += 1;
-      // TODO: get a new set of colors somehow
-      rgb = "steelblue";
-      cx = ix + Math.round((0.5 - Math.random()) * (100 - params.dotregularity)); // sx in coleman's code
-      cy = iy + Math.round((0.5 - Math.random()) * (100 - params.dotregularity)); // sy in coleman's code
-      rx = params.dotsize; // tx in coleman's code
-      ry = params.dotsize; // ty in coleman's code
-      /*
-      console.log(rx);
-      console.log(ry);
-      */
-      // TODO: envelope code here
+  if(!(params.random)) {
+    for(iy=params.dotsize+params.dY; iy<dots_height; iy += params.dotspace) {
+      for(ix=params.dotsize+params.dX; ix<dots_width; ix += params.dotspace) {
+        //counter += 1;
+        rgb = get_random_htmlcolor_fromimage();
+        //console.log(rgb);
+        cx = ix + Math.round((0.5 - Math.random()) * (100 - params.dotregularity)); // sx in coleman's code
+        cy = iy + Math.round((0.5 - Math.random()) * (100 - params.dotregularity)); // sy in coleman's code
+        rx = params.dotsize; // tx in coleman's code
+        ry = params.dotsize; // ty in coleman's code
+        /*
+        console.log(rx);
+        console.log(ry);
+        */
+        // TODO: envelope code here
+        if(params.envelope1) {
+        }
+        else if(params.envelope2) {
+          
+        }
+ 
+        // This looks backwards to me, but it's how it is in Coleman's code
+        if(params.multibooble) {
+          drawBooble(cx, cy, rx, ry, rgb, params);
+        }
+        else {
+          drawDot(cx, cy, rx, ry, rgb, params);
+          //drawDot(cx, cy, rx, ry, rgb, params.outlines);
+        }
+      }
+    }
+  }
+  else {
+    for(i=0; i<500; i++) {
+      cx = Math.random()*dots_width;
+      cy = Math.random()*dots_height;
+      rgb = get_random_htmlcolor_fromimage();
+      rx = params.dotsize;
+      ry = params.dotsize;
+      console.log
       if(params.envelope1) {
+        //cx = Math.sin(
       }
       else if(params.envelope2) {
+        cx = Math.sin(i)*cx; 
+        cy = cx;
       }
-
-      // This looks backwards to me, but it's how it is in Coleman's code
-      if(params.multibooble) {
+      if(params.multibooble == 1) {
         drawDot(cx, cy, rx, ry, rgb, params.outlines);
       }
       else {
-        drawBooble(cx, cy, rx, ry, rgb, params.outlines);
+        drawDot(cx, cy, rx, ry, rgb, params.outlines);
       }
     }
   }
@@ -274,6 +343,22 @@ console.log("called dot_analyze");
   //svg.append("svg:circle").attr("cx", 200).attr("cy",200).attr("r",20).style("stroke","#777777").style("fill","black");
 }
 
+function get_random_htmlcolor_fromimage() 
+{
+  // array contains rgb and alpha values in order, so 4 slots per pixel
+  //console.log(imgpixel);
+  pixel = Math.random()*(imgpixel.length/4);
+  idx   = Math.round(pixel)*4;
+
+  red   = imgpixel[idx];
+  green = imgpixel[idx+1];
+  blue  = imgpixel[idx+2];
+
+  ret = "rgb("+red+","+green+","+blue+")";
+
+  return(ret);
+}
+
 // Called after the file selection button is clicked and a file is chosen
 function handleFileSelect(evt) {
   var files = evt.target.files; // FileList object
@@ -297,12 +382,16 @@ function handleFileSelect(evt) {
       // Draw the image onto the canvas then read it back in
       // You *have* to use the onload method to draw the image -- once again, the asynchronous "bug"!
       // http://stackoverflow.com/questions/4773966/drawing-an-image-from-a-data-url-to-a-canvas
+      //console.log(context);
       var img     = new Image();
       img.src     = filedata;
       img.onload  = function() {
         context.drawImage(img, 0, 0);
-        data = context.getImageData(25, 25, 10, 10);
-        console.log(data);
+        // This gets *all* pixels into an array (why not?)
+        imgpixel = context.getImageData(0, 0, img.width, img.height).data;
+        // right here is where we can actually enable the go button as the image data have been loaded completely
+        Ext.getCmp('gobutton').enable();
+        //get_random_htmlcolor();
       }
       //console.log(filedata);
       // This doesn't seem to work, either because the image isn't actually being rendered on the webpage 
